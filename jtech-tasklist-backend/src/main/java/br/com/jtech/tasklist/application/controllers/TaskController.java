676b912ceda32bd1dd35a75be1e6fba762e89831
FabioSimones <simones.fabio1994@gmail.com>
2026-01-15 19:32:37 -0300

    Criando o projeto frontend.

--- jtech-tasklist-backend/src/main/java/br/com/jtech/tasklist/application/controllers/TaskController.java
+++ jtech-tasklist-backend/src/main/java/br/com/jtech/tasklist/application/controllers/TaskController.java
@@ -14,6 +14,7 @@ import jakarta.validation.Valid;
 import java.util.List;
 import java.util.stream.Collectors;
 
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 @RequestMapping("/tasks")
 @RequiredArgsConstructor
